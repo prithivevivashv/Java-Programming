800973ce00b350de49cc96067a1a5b2b60912ea9
prithivevivashv <vpvivash@gmail.com>
2026-07-16 20:26:58 +0530

    Create Zigzag_pattern.java

---
+++ Day6/Zigzag_pattern.java
@@ -0,0 +1,14 @@
+package DAY6;
+
+public class Zigzag_pattern {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++) {
+            int n=i;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(n+" ");
+                n=n+5-j;
+            }
+            System.out.println();
+        }
+    }
+}
