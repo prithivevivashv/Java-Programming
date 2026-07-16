38e718692da88696cabc77c062cd7158719c08cd
prithivevivashv <vpvivash@gmail.com>
2026-07-16 20:22:45 +0530

    Create Floyds_triangle.java

---
+++ Day6/Floyds_triangle.java
@@ -0,0 +1,13 @@
+package DAY6;
+
+public class Floyds_triangle {
+    public static void main(String[] args){
+    int n=13;
+    for(int i=1;i<=n;i++) {
+        for (int j = 1; j <= n; j++) {
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
+}
