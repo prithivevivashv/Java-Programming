9f8b1ba657949785c83abdbee94d02b98e8ab0d3
prithivevivashv <vpvivash@gmail.com>
2026-07-16 20:24:06 +0530

    Create Multiplication_table.java

---
+++ Day6/Multiplication_table.java
@@ -0,0 +1,14 @@
+package DAY6;
+
+public class Multiplication_table {
+    public static void main(String[] args)
+    {
+        int n=5;
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
