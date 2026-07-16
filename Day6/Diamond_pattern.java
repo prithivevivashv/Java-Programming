80e7f6bdfc746d2c0a2482f226f32aac3622a3e7
prithivevivashv <vpvivash@gmail.com>
2026-07-16 20:22:21 +0530

    Create Diamond_pattern.java

---
+++ Day6/Diamond_pattern.java
@@ -0,0 +1,21 @@
+package DAY6;
+
+public class Diamond_pattern {
+    public static void main(String[] args){int num=5;
+        for(int i=1;i<=num;i++)
+        {
+            for(int j=1;j<=num-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=num-1;i>=1;i--){
+            for (int j=1;j<=num-i;j++)System.out.print(" ");
+            for(int k=1;k<=2*i-1;k++)System.out.print("*");
+            System.out.println();
+        }
+    }
+}
