461e70b649f25387151fe03fbe211a567ff676bb
prithivevivashv <vpvivash@gmail.com>
2026-07-16 20:24:44 +0530

    Create Number_Triangle.java

---
+++ Day6/Number_Triangle.java
@@ -0,0 +1,13 @@
+package DAY6;
+
+public class Number_Triangle {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
