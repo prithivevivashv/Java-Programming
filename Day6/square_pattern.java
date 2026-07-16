1f743b3a665e1d96883516dd83056ad9c6677c1b
prithivevivashv <vpvivash@gmail.com>
2026-07-16 20:25:33 +0530

    Create square_pattern.java

---
+++ Day6/square_pattern.java
@@ -0,0 +1,12 @@
+package DAY6;
+public class square_pattern {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
