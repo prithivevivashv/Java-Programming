5eb81b8e8c4e5a71168c344eb6890b9c335b1ba4
prithivevivashv <vpvivash@gmail.com>
2026-07-16 20:23:34 +0530

    Create Inverted_triangle.java

---
+++ Day6/Inverted_triangle.java
@@ -0,0 +1,13 @@
+package DAY6;
+
+public class Inverted_triangle {
+    public static void main(String[] args){
+    int n=5;
+    for(int i=1;i<=n;i++) {
+        for (int j = n; j>i; j--) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
