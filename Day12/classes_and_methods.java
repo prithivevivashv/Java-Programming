11e877b058f9d1aebfe1e1878c978fc504b5afca
prithivevivashv <vpvivash@gmail.com>
2026-07-23 10:45:21 +0530

    Create classes_and_methods.java

---
+++ Day12/classes_and_methods.java
@@ -0,0 +1,14 @@
+package DAY12;
+class B{
+    int age;
+    String name;
+    void display(){
+        System.out.println("Display");
+    }
+}
+public class classes_and_methods {
+    public  static  void  main(String[] args){
+        B a=new B();
+        a.display();
+    }
+}
